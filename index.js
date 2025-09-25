aa61252706a7d9129e8bb9185c19da6257496d80
chavitinibalaji <balajichavitini@gmail.com>
2025-09-25 15:33:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var http = require('http');
 
+//create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to DevOps Training'); // write response
-  res.end(); // end response
-}).listen(3000, '0.0.0.0', () => {
-  console.log('Server running on port 3000');
-});
+  res.write('Welcome to DevOps Training'); //write a response to the client
+  res.end(); //end the response
+}).listen(81); //the server object listens on port 80
  
